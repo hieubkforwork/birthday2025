664936225e1bc95570268f61ecb8d36ff63cc1b0
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2026-01-16 22:40:21 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const CORRECT_PASSWORD = '24102025';
 // Note: Add your happy-birthday.mp3 file to src/assets/
 // Using a placeholder path - will work once file is added
 const MUSIC_PATH = '/happy-birthday.mp3';
-;
+
 
 function App() {
   const [currentScene, setCurrentScene] = useState('password'); // 'password', 'cake', 'birthday'
